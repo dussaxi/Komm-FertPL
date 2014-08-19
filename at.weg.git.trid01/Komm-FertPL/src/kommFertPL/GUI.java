34d1cd70686326474e7ca0c873347de4aa51c465
dussaxi <matthias.weg@gmx.at>
2014-08-19 16:29:43 +0200

    Table filled with DB Data

--- at.weg.git.trid01/Komm-FertPL/src/kommFertPL/GUI.java
+++ at.weg.git.trid01/Komm-FertPL/src/kommFertPL/GUI.java
@@ -2,9 +2,14 @@ package kommFertPL;
 
 import java.awt.EventQueue;
 
+import javax.sql.RowSetEvent;
+import javax.sql.RowSetListener;
+import javax.sql.rowset.CachedRowSet;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
@@ -25,6 +30,8 @@ import javax.swing.JButton;
 import javax.swing.AbstractAction;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
 
@@ -36,12 +43,14 @@ import java.io.*;
 
 import javax.swing.JFormattedTextField;
 
+import com.sun.rowset.CachedRowSetImpl;
+
 import java.util.Date;
+import java.util.Vector;
 import java.text.SimpleDateFormat;
 import java.awt.Font;
 
-
-public class GUI extends JFrame {
+public class GUI extends JFrame implements RowSetListener {
 	
 	/**
 	 * 
@@ -78,6 +87,9 @@ public class GUI extends JFrame {
 	private Statement stmt = null;
 	private ResultSet rs = null;
 	
+	QueryTableModel myQueryTableModel;
+	JTable table;
+	
 		
 	/**
 	 * Launch the application.
@@ -87,6 +99,7 @@ public class GUI extends JFrame {
 			public void run() {
 				try {
 					GUI frame = new GUI();
+					frame.pack();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -99,12 +112,29 @@ public class GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GUI() {
+		// Set Windows title
+		super("Interface Fertigung - Prüflabor");
+		connectToDB();
+		
+		// Close connections exit the application when the user
+		// closes the window
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				try {
+					con.close();
+				} catch (SQLException sqle) {
+					printSQLException(sqle);
+				}
+				System.exit(0);
+			}
+		});
+
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 254);
 		Container contentPane = getContentPane();
-		JPanel panel = new JPanel();
-		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
 		
 //		setContentPane(contentPane);
 //		GridBagLayout gbl_contentPane = new GridBagLayout();
@@ -144,6 +174,7 @@ public class GUI extends JFrame {
 		gbc_lblFertigungsauftrag.anchor = GridBagConstraints.EAST;
 		gbc_lblFertigungsauftrag.gridx = 0;
 		gbc_lblFertigungsauftrag.gridy = 0;
+		//gbc_lblFertigungsauftrag.fill = GridBagConstraints.HORIZONTAL;
 		panelInput.add(lblFertigungsauftrag, gbc_lblFertigungsauftrag);
 		
 		txtFertigungsauftrag = new JTextField();
@@ -161,8 +192,9 @@ public class GUI extends JFrame {
 		gbc_lblAnzahl.gridwidth = 1;
 		gbc_lblAnzahl.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAnzahl.anchor = GridBagConstraints.EAST;
-		gbc_lblAnzahl.gridx = 1;
-		gbc_lblAnzahl.gridy = 0;
+		gbc_lblAnzahl.gridx = 0;
+		gbc_lblAnzahl.gridy = 1;
+		//gbc_lblAnzahl.fill = GridBagConstraints.HORIZONTAL;
 		panelInput.add(lblAnzahl, gbc_lblAnzahl);
 		
 		txtAnzahl = new JTextField();
@@ -181,8 +213,9 @@ public class GUI extends JFrame {
 		gbc_lblAnlage.gridwidth = 1;
 		gbc_lblAnlage.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAnlage.anchor = GridBagConstraints.EAST;
-		gbc_lblAnlage.gridx = 2;
-		gbc_lblAnlage.gridy = 0;
+		gbc_lblAnlage.gridx = 0;
+		gbc_lblAnlage.gridy = 2;
+		//gbc_lblAnlage.fill = GridBagConstraints.HORIZONTAL;
 		panelInput.add(lblAnlage, gbc_lblAnlage);
 		
 		txtAnlage = new JTextField();
@@ -191,8 +224,8 @@ public class GUI extends JFrame {
 		gbc_txtAnlage.gridwidth = 1;
 		gbc_txtAnlage.insets = new Insets(0, 0, 5, 5);
 		gbc_txtAnlage.anchor = GridBagConstraints.WEST;
-		gbc_txtAnlage.gridx = 2;
-		gbc_txtAnlage.gridy = 1;
+		gbc_txtAnlage.gridx = 1;
+		gbc_txtAnlage.gridy = 2;
 		panelInput.add(txtAnlage, gbc_txtAnlage);
 		txtAnlage.setColumns(10);
 		
@@ -201,8 +234,9 @@ public class GUI extends JFrame {
 		gbc_lblDatum.gridwidth = 1;
 		gbc_lblDatum.insets = new Insets(0, 0, 5, 5);
 		gbc_lblDatum.anchor = GridBagConstraints.EAST;
-		gbc_lblDatum.gridx = 3;
-		gbc_lblDatum.gridy = 0;
+		gbc_lblDatum.gridx = 0;
+		gbc_lblDatum.gridy = 3;
+		//gbc_lblDatum.fill = GridBagConstraints.HORIZONTAL;
 		panelInput.add(lblDatum, gbc_lblDatum);
 		
 		txtDatum = new JFormattedTextField(new SimpleDateFormat("d.M.yyyy"));
@@ -211,8 +245,8 @@ public class GUI extends JFrame {
 		gbc_txtDatum.gridwidth = 1;
 		gbc_txtDatum.insets = new Insets(0, 0, 5, 5);
 		gbc_txtDatum.anchor = GridBagConstraints.WEST;
-		gbc_txtDatum.gridx = 3;
-		gbc_txtDatum.gridy = 1;
+		gbc_txtDatum.gridx = 1;
+		gbc_txtDatum.gridy = 3;
 		panelInput.add(txtDatum, gbc_txtDatum);
 		txtDatum.setColumns(10);
 		
@@ -221,8 +255,9 @@ public class GUI extends JFrame {
 		gbc_lblZeitAnlieferung.gridwidth = 1;
 		gbc_lblZeitAnlieferung.insets = new Insets(0, 0, 5, 5);
 		gbc_lblZeitAnlieferung.anchor = GridBagConstraints.EAST;
-		gbc_lblZeitAnlieferung.gridx = 4;
-		gbc_lblZeitAnlieferung.gridy = 0;
+		gbc_lblZeitAnlieferung.gridx = 0;
+		gbc_lblZeitAnlieferung.gridy = 4;
+		//gbc_lblZeitAnlieferung.fill = GridBagConstraints.HORIZONTAL;
 		panelInput.add(lblZeitAnlieferung, gbc_lblZeitAnlieferung);
 		
 		txtZeitAnlieferung = new JFormattedTextField(new SimpleDateFormat("H:mm"));
@@ -231,8 +266,8 @@ public class GUI extends JFrame {
 		gbc_txtZeitAnlieferung.gridwidth = 1;
 		gbc_txtZeitAnlieferung.insets = new Insets(0, 0, 5, 5);
 		gbc_txtZeitAnlieferung.anchor = GridBagConstraints.WEST;
-		gbc_txtZeitAnlieferung.gridx = 4;
-		gbc_txtZeitAnlieferung.gridy = 1;
+		gbc_txtZeitAnlieferung.gridx = 1;
+		gbc_txtZeitAnlieferung.gridy = 4;
 		panelInput.add(txtZeitAnlieferung, gbc_txtZeitAnlieferung);
 		txtZeitAnlieferung.setColumns(10);
 		
@@ -241,8 +276,9 @@ public class GUI extends JFrame {
 		gbc_lblNameMA.gridwidth = 1;
 		gbc_lblNameMA.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNameMA.anchor = GridBagConstraints.EAST;
-		gbc_lblNameMA.gridx = 5;
-		gbc_lblNameMA.gridy = 0;
+		gbc_lblNameMA.gridx = 0;
+		gbc_lblNameMA.gridy = 5;
+		//gbc_lblNameMA.fill = GridBagConstraints.HORIZONTAL;
 		panelInput.add(lblNameMA, gbc_lblNameMA);
 		
 		txtNameMA = new JTextField();
@@ -251,8 +287,8 @@ public class GUI extends JFrame {
 		gbc_txtNameMA.gridwidth = 1;
 		gbc_txtNameMA.insets = new Insets(0, 0, 5, 5);
 		gbc_txtNameMA.anchor = GridBagConstraints.WEST;
-		gbc_txtNameMA.gridx = 5;
-		gbc_txtNameMA.gridy = 1;
+		gbc_txtNameMA.gridx = 1;
+		gbc_txtNameMA.gridy = 5;
 		panelInput.add(txtNameMA, gbc_txtNameMA);
 		txtNameMA.setColumns(10);
 		
@@ -261,6 +297,21 @@ public class GUI extends JFrame {
 		btnDatenSpeichern = new JButton("Daten Speichern");
 		btnDatenSpeichern.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				fertigungsauftrag = txtFertigungsauftrag.getText();
+				anzahl = txtAnzahl.getText();
+				anlage = txtAnlage.getText();
+				SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+				datum = DATE_FORMAT.format(txtDatum.getValue());
+				SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
+				zeitMeldung = TIME_FORMAT.format(txtZeitAnlieferung.getValue());
+				nameMA = txtNameMA.getText();		
+				if (checkConditions()) {
+					// Data is OK, now save the Data.
+					// writeToFile();
+				    saveToDB();
+					System.out.println("Done");
+					table.setModel(myQueryTableModel);
+				}
 			}
 		});
 		btnDatenSpeichern.setAction(action);
@@ -268,19 +319,161 @@ public class GUI extends JFrame {
 		
 		JPanel panelTable = new JPanel(new BorderLayout());
 		
-		String[] columnNames = {"Fertigungsauftrag", "Anzahl Geräte", "Anlage", "Datum der Anlieferung", "Zeit der Anlieferung", "Name des Mitarbeiters"};
-		Object[][] data = {{"1234567", 10, "TEST1A", "25.7.2014", "11:11", "Matthias Weg"}}; 
-		JTable table = new JTable(data, columnNames);
-		JScrollPane spTable = new JScrollPane(table);
+	    CachedRowSet myCachedRowSet;
+		try {
+			myCachedRowSet = getContentsOfQueryTable();
+			myQueryTableModel = new QueryTableModel(myCachedRowSet);
+		    myQueryTableModel.addEventHandlersToRowSet(this);
+		} catch (SQLException e1) {
+			printSQLException(e1);
+		}	    
+
+	    table = new JTable(); // Displays the table
+	    table.setModel(myQueryTableModel);
+		
+	    JScrollPane spTable = new JScrollPane(table);
 		table.setFillsViewportHeight(true);
+		panelTable.add(spTable, BorderLayout.CENTER);
 		
 		JPanel panelCopyright = new JPanel(new FlowLayout());
 		
 		JLabel lblcopyright = new JLabel("Vers. 1.1 \u00A9 Matthias Weg, 5.8.2014");
 		lblcopyright.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblcopyright.setHorizontalAlignment(SwingConstants.EAST);
+		//lblcopyright.setHorizontalAlignment(SwingConstants.EAST);
 		panelCopyright.add(lblcopyright);
 		
+		contentPane.add(panelInput);
+		contentPane.add(panelButtons);
+		contentPane.add(panelTable);
+		contentPane.add(panelCopyright);
+	}
+	
+	// From http://docs.oracle.com/javase/tutorial/uiswing/components/table.html#data
+	// and http://www.java2s.com/Code/Java/Swing-JFC/DisplayResultSetinTableJTable.htm
+    class QueryTableModel extends AbstractTableModel {
+    	CachedRowSet myRowSet; // The ResultSet to interpret
+    	ResultSetMetaData metadata; // Additional information about the results
+    	int numcols, numrows; // How many rows and columns in the table
+ 
+    	public CachedRowSet getCoffeesRowSet() {
+    		return myRowSet;
+    	}
+    	
+        public QueryTableModel(CachedRowSet rowSetArg) throws SQLException {
+        	this.myRowSet = rowSetArg;
+            this.metadata = this.myRowSet.getMetaData();
+            numcols = metadata.getColumnCount();
+
+            // Retrieve the number of rows.
+            this.myRowSet.beforeFirst();
+            this.numrows = 0;
+            while (this.myRowSet.next()) {
+              this.numrows++;
+            }
+            this.myRowSet.beforeFirst();
+        }
+        
+        public void addEventHandlersToRowSet(RowSetListener listener) {
+        	this.myRowSet.addRowSetListener(listener);
+        }
+        
+        public void close() {
+        	try {
+        		myRowSet.getStatement().close();
+        	} catch (SQLException e) {
+        		printSQLException(e);
+        	}
+        }
+
+        /** Automatically close when we're garbage collected */
+        protected void finalize() {
+        	close();
+        }
+
+        public int getColumnCount() {
+            return numcols;
+        }
+ 
+        public int getRowCount() {
+            return numrows;
+        }
+ 
+        public String getColumnName(int col) {
+        	try {
+        		return this.metadata.getColumnLabel(col + 1);
+        	} catch (SQLException e) {
+        		return e.toString();
+        	}
+        }
+ 
+        public Object getValueAt(int row, int col) {
+        	try {
+        		this.myRowSet.absolute(row + 1);
+        		Object o = this.myRowSet.getObject(col + 1);
+        		if (o == null)
+        			return null;
+        		else
+        			return o.toString();
+        	} catch (SQLException e) {
+        		return e.toString();
+        	}
+        }
+ 
+        /*
+         * JTable uses this method to determine the default renderer/
+         * editor for each cell.  If we didn't implement this method,
+         * then the last column would contain text ("true"/"false"),
+         * rather than a check box.
+         */
+        public Class getColumnClass(int c) {
+            return String.class;
+        }
+ 
+        /*
+         * Don't need to implement this method unless your table's
+         * editable.
+         */
+        public boolean isCellEditable(int row, int col) {
+            //Note that the data/cell address is constant,
+            //no matter where the cell appears onscreen.
+//            if (col < 2) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+        	return false;
+        }
+ 
+        /*
+         * Don't need to implement this method unless your table's
+         * data can change.
+         */
+        public void setValueAt(Object value, int row, int col) {
+        	System.out.println("Calling setValueAt row " + row + ", column " + col);
+        }
+ 
+        public void addTableModelListener(TableModelListener l) {
+        }
+
+        public void removeTableModelListener(TableModelListener l) {
+        }
+    }
+    
+	public CachedRowSet getContentsOfQueryTable() throws SQLException {
+        CachedRowSet crs = null;
+        try {
+        	crs = new CachedRowSetImpl();
+        	crs.setType(ResultSet.TYPE_SCROLL_INSENSITIVE);
+        	crs.setConcurrency(ResultSet.CONCUR_UPDATABLE);
+        	crs.setUsername("QSYSTEST");
+        	crs.setPassword("qsys");
+        	crs.setUrl("jdbc:oracle:thin:@atdotrsr26:1521/UNITEST");
+        	crs.setCommand("select * from tri_pl_geraete order by id desc");
+        	crs.execute();
+        } catch (SQLException e) {
+        	printSQLException(e);
+        }
+        return crs;
 	}
 
 	private class SwingAction extends AbstractAction {
@@ -293,24 +486,7 @@ public class GUI extends JFrame {
 			putValue(SHORT_DESCRIPTION, "Speichern der eingegebenen Daten in CAQ");
 		}
 		public void actionPerformed(ActionEvent e) {
-			fertigungsauftrag = txtFertigungsauftrag.getText();
-			anzahl = txtAnzahl.getText();
-			anlage = txtAnlage.getText();
-			SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-			datum = DATE_FORMAT.format(txtDatum.getValue());
-			SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
-			zeitMeldung = TIME_FORMAT.format(txtZeitAnlieferung.getValue());
-			nameMA = txtNameMA.getText();		
 			
-			if (checkConditions()) {
-				// Data is OK, now save the Data.
-				
-				// writeToFile();
-			    saveToDB();
-				
-				System.out.println("Done");
-				
-			}
 		}
 	}
 	
@@ -365,14 +541,11 @@ public class GUI extends JFrame {
 	}
 
 	private void saveToDB() {
-		if (connectToDB() == true) {
-			insertData(); 
-			closeConnection();
-			JOptionPane.showMessageDialog(null,
-				    "Daten erfolgreich gespeichert!",
-				    "Data saved",
-				    JOptionPane.INFORMATION_MESSAGE);
-		}
+		insertData(); 
+		JOptionPane.showMessageDialog(null,
+			    "Daten erfolgreich gespeichert!",
+			    "Data saved",
+			    JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	private void insertData() {
@@ -474,4 +647,62 @@ public class GUI extends JFrame {
 		  return str.length() == pos.getIndex();
 	  }
 	}
+	
+	public static boolean ignoreSQLException(String sqlState) {
+		if (sqlState == null) {
+			System.out.println("The SQL state is not defined!");
+			return false;
+		}
+		// X0Y32: Jar file already exists in schema
+		if (sqlState.equalsIgnoreCase("X0Y32"))
+			return true;
+		// 42Y55: Table already exists in schema
+		if (sqlState.equalsIgnoreCase("42Y55"))
+			return true;
+		return false;
+	}
+	
+	public static void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				if (ignoreSQLException(((SQLException)e).getSQLState()) == false) {
+					e.printStackTrace(System.err);
+					System.err.println("SQLState: " + ((SQLException)e).getSQLState());
+					System.err.println("Error Code: " + ((SQLException)e).getErrorCode());
+					System.err.println("Message: " + e.getMessage());
+					Throwable t = ex.getCause();
+					while (t != null) {
+						System.out.println("Cause: " + t);
+						t = t.getCause();
+					}
+				}
+			}
+		}
+	}
+	
+	public void actionPerformed(ActionEvent event) {  }
+
+	public void rowSetChanged(RowSetEvent event) {  }
+
+	public void rowChanged(RowSetEvent event) {
+		CachedRowSet currentRowSet = this.myQueryTableModel.myRowSet;
+		try {
+			currentRowSet.moveToCurrentRow();
+			myQueryTableModel = new QueryTableModel(myQueryTableModel.getCoffeesRowSet());
+			table.setModel(myQueryTableModel);
+		} catch (SQLException ex) {
+			printSQLException(ex);
+			// Display the error in a dialog box.
+			JOptionPane.showMessageDialog(
+					GUI.this,
+					new String[] { // Display a 2-line message
+							ex.getClass().getName() + ": ",
+							ex.getMessage()
+					}
+			);
+		}
+	}
+
+	public void cursorMoved(RowSetEvent event) {  }
+	
 }
